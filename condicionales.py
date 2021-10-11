be06e5b350bfdf71c6898ab892c5b12ebee0ef47
Carlos Castillo <ext.ccastillo@servipag.cl>
2021-10-11 19:13:09 -0300

    ejercicios basicos

---
+++ condicionales.py
@@ -0,0 +1,15 @@
+edad = int(input('Escribe tu edad: '))
+
+if edad > 17:
+    print('Eres mayor de edad')
+else:
+    print('Eres menor de edad')
+
+numero = int(input('Escribe un número'))
+
+if numero > 5:
+    print('Es mayor a cinco')
+elif numero == 5:
+    print('Es igual a cinco')
+else:
+    print('E$s menor a cinco')
---
+++ conversor.py
@@ -0,0 +1,6 @@
+pesos = input('¿Cuantos pesos colombianos tienes?: ')
+pesos = float(pesos)
+valor_dolar = 3875
+dolares = round(pesos/valor_dolar, 2)
+dolares = str(dolares)
+print('Tienes $'+dolares+' dólares')
