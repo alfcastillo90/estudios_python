62cd329e46b3de4da0b36699758179b440c0bd8c
Carlos Castillo <ext.ccastillo@servipag.cl>
2021-10-11 20:35:52 -0300

    modularizando el conversor de monedas

--- conversor.py
+++ conversor.py
@@ -1,3 +1,10 @@
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input('¿Cuantos pesos '+tipo_pesos+' tienes?: ')
+    pesos = float(pesos)
+    dolares = round(pesos / valor_dolar, 2)
+    dolares = str(dolares)
+    print('Tienes $' + dolares + ' dólares')
+
 menu = """
     Bienvenido al conversor de monedas 
     
@@ -10,25 +17,10 @@ menu = """
 opcion = int(input(menu))
 
 if opcion == 1:
-    pesos = input('¿Cuantos pesos colombianos tienes?: ')
-    pesos = float(pesos)
-    valor_dolar = 3875
-    dolares = round(pesos / valor_dolar, 2)
-    dolares = str(dolares)
-    print('Tienes $' + dolares + ' dólares')
+    conversor('colombianos', 3875)
 elif opcion == 2:
-    pesos = input('¿Cuantos pesos argentinos tienes?: ')
-    pesos = float(pesos)
-    valor_dolar = 182
-    dolares = round(pesos / valor_dolar, 2)
-    dolares = str(dolares)
-    print('Tienes $' + dolares + ' dólares')
+    conversor('argentinos', 182)
 elif opcion == 3:
-    pesos = input('¿Cuantos pesos mexicanos tienes?: ')
-    pesos = float(pesos)
-    valor_dolar = 20.87
-    dolares = round(pesos / valor_dolar, 2)
-    dolares = str(dolares)
-    print('Tienes $' + dolares + ' dólares')
+    conversor('mexicanos', 20.87)
 else:
     print('Opción invalida')
