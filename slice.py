6ff2e86b65f09b34b9d21bb3611f7bcc24e5e5e4
Carlos Castillo <ext.ccastillo@servipag.cl>
2021-10-11 21:34:57 -0300

    ejemplo de slices en python

---
+++ slice.py
@@ -0,0 +1,9 @@
+nombre = 'Francisco'
+print(nombre[0])
+print(nombre[1])
+print(nombre[0:3])
+print(nombre[:2])
+print(nombre[3:])
+print(nombre[1:7])
+print(nombre[1:7:2])
+print(nombre[::-1])# al colocar esto, python imprime los textos en orden inverso
