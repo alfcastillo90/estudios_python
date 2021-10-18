671499c02dcd192b4883f3cc55761ab4b9cdf66a
Carlos Castillo <ext.ccastillo@servipag.cl>
2021-10-18 19:17:45 -0300

    ADD: ejemplo de sentencias break, continue y ciclos

---
+++ break.py
@@ -0,0 +1,21 @@
+def run():
+    for contador in range(100):
+        if contador % 2 != 0:
+            continue
+        print(contador)
+
+    for i in range(20):
+        print(i)
+        if i == 15:
+            break
+
+    texto = input('Escribe un texto: ')
+
+    for letra in texto:
+        if letra == 'o':
+            break
+        print(letra)
+
+
+if __name__ == '__main__':
+    run()
---
+++ for.py
@@ -0,0 +1,8 @@
+for contador in range(1000):
+    print(contador)
+
+for contador in range(1, 1001):
+    print(contador)
+
+for i in range(10):
+    print(11*i)
